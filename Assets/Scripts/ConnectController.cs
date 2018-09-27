8628a634ea1cb6e80a954ee6d48a98bb2938f9b4
mascut-0614 <chocochan_i@dcl.cs.waseda.ac.jp>
2018-09-27 11:24:07 +0900

    Window&Connect function is finished

---
+++ Assets/Scripts/ConnectController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Net;
+using System.Net.Sockets;
+using UnityEngine.UI;
+
+
+public class ConnectController : MonoBehaviour {
+	TcpClient tcpClient = new TcpClient();
+	NetworkStream ns;
+	System.IO.MemoryStream ms;
+    public InputField inputField;
+    public GameObject ConnectArea;
+
+	public void ConnectStart(string iptext){
+		try{
+			tcpClient.Connect(iptext, 8080);
+			ns = tcpClient.GetStream();
+            inputField.text = "Successed";
+            ConnectArea.gameObject.SetActive(false);
+		}catch{
+            inputField.text = "Failed";
+		}
+	}
+
+    public void SendMes(string mes){
+        if(tcpClient.Connected){
+            System.Text.Encoding enc = System.Text.Encoding.UTF8;
+            byte[] sendBytes = enc.GetBytes(mes + '\n');
+            ns.Write(sendBytes, 0, sendBytes.Length);
+        }
+    }
+
+    void Update()
+    {
+        if (tcpClient.Connected)
+        {
+            if (tcpClient.Available > 0)
+            {
+                ms = new System.IO.MemoryStream();
+                byte[] resBytes = new byte[256];
+                int resSize = 0;
+                resSize = ns.Read(resBytes, 0, resBytes.Length);
+                if (resSize == 0)
+                {
+                    Debug.Log("サーバーが切断されました。");
+                    ms.Close();
+                    ns.Close();
+                    tcpClient.Close();
+                }
+                ms.Write(resBytes, 0, resSize);
+                System.Text.Encoding enc = System.Text.Encoding.UTF8;
+                string resMsg = enc.GetString(ms.GetBuffer(), 0, (int)ms.Length);
+                ms.Close();
+                string[] lineArrays = resMsg.Split('|');
+                foreach (string lineArray in lineArrays)
+                {
+                    if (lineArray != "")
+                    {
+                        //FindObjectOfType<ChatScreen>().UpdateChat(lineArray);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PriMesWindow.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PriMesWindow : MonoBehaviour {
+    public Text[] texts = new Text[11];
+    public void Start()
+    {
+        foreach (Text chattext in texts)
+        {
+            chattext.text = "";
+        }
+    }
+    public void UpdatePriMesChat(string mes)
+    {
+        int i = 0;
+        while (texts[i].text != "")//空きを確認
+        {
+            i++;
+            if (i == 11)
+            {
+                break;
+            }
+        }
+        if (i < 11)
+        {
+            texts[i].text = mes;
+        }
+        else//埋まっていたら詰める
+        {
+            for (int j = 0; j < 10; j++)
+            {
+                texts[j].text = texts[j + 1].text;
+            }
+            texts[10].text = mes;
+        }
+    }
+}
